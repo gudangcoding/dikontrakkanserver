9b32de2786c56c97f46729546300143459f2037d
gudang coding <dientrimedia@gmail.com>
2023-03-14 22:16:40 +0700

    update fungsi upload

--- admina/inc/uploadbase64.php
+++ admina/inc/uploadbase64.php
@@ -9,16 +9,18 @@
     //function to upload image and get an unique name to store in db
 
     function uploadbase64image($base64) {
-        $direktori = "https://kontrakan.tukangrenov.id/api/upload/"; 
+        $direktori = "upload/"; 
         //uniqid(). date("Y-m-d-H-i-s") . ".png";
         $uniname = $direktori.date("Y-m-d-H-i-s") . ".png";
-        $new_image_url ="upload/". $uniname;
-        $base641 = 'data:image/png;base64,' . $base64;
+        $new_image_url =  $uniname;
+        // $base641 = 'data:image/png;base64,' . $base64;
         $base642 = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64));
         //$image = imagecreatefromstring($base642);
         file_put_contents($new_image_url, $base642);
         return $uniname;
     }
+
+    
 
 $sql = "INSERT INTO product SET gambar='$unique_name'";
 if ($post) {
